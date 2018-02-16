1eacb9da385fef4c54d72b653689838a740738a7
Filipe Rasoilo <3279118+fRasoilo@users.noreply.github.com>
2018-02-16 18:42:50 +0000

    Examples in simple_plugin.cpp and license in simple_plugin.h

--- simple_plugin.cpp
+++ simple_plugin.cpp
@@ -4,34 +4,84 @@
 
 #include <stdio.h>
 
+
 #include "sample_plugin.h"
 
 
-#include "simple_plugin.h"
+
+#include "sample_plugin.h"
 
 char* sample_plugin = "sample_plugin.dll";
-char* second_plugin = "second_plugin.dll";
-char* third_plugin = "third_plugin.dll";
-char* fourth_plugin = "fourth_plugin.dll";
 
 
 int main()
 {
 
-    //Sample Plugin
-    bool32 result = sp_win32_load_plugin(plugin_name, true);
-    plugin_example_api *pe = (plugin_example_api*)sp_registry.get(PLUGIN_EXAMPLE_API_NAME);
-    pe->my_print();
-    pe->my_add_and_print(2,3);
-   
-    //Loop forever until we break out. This is used to demonstrate the hot-reloading option.
+    //Simplest API 
+    //Let the library manage the plugin, we load the plugin we want and then request it's API from the library.
+    //We don't need to keep track of the plugin.
+    bool32 reloadable = true;
+    sp_load_plugin(plugin_name, reloadable);
+    sample_plugin_api *sample_api = (sample_plugin_api*)sp_get_api(SAMPLE_PLUGIN_API_NAME);
+    sample_api->my_print();
+    sample_api->my_add_and_print(2,3);
+
+    //If we want we can also keep a pointer to the plugin. The library will still keep track of it internally, but we can access the plugin directly.
+    SPlugin *plugin = sp_load_plugin(second_plugin, !reloadable);
+    second_plugin_api *second_api = (second_plugin_api*)sp_get_api(plugin);
+    second_api->my_second_print();
+    
+    //If we know we are done with a plugin, we can unload it manually. Otherwise leave it for the library to clean up at shutdown time.
+    sp_unload_plugin(plugin);
+    //We can also unload it trough the API name
+    sp_unload_plugin(SAMPLE_PLUGIN_API_NAME);
+
+    //
+    //Using our registry
+    //
+
+    //The examples above use the default registry provided by the library.
+    //You can also create instances of an APIRegistry and use them instead of the default library one.
+    //For example if you would like to create a separete registry for different kinds of plugins.
+    APIRegistry my_registry = sp_registry_create(args); //capacity
+    
+    //Now you can do all the same operations as above passing in your created registry.
+    sp_load_plugin(&my_registry,plugin_name, reloadable);
+    sample_plugin_api *sample_api = (sample_plugin_api*)sp_get_api(&my_registry,SAMPLE_PLUGIN_API_NAME);
+    sample_api->my_print();
+    sample_api->my_add_and_print(2,3);
+
+
+    plugin = sp_load_plugin(second_plugin, !reloadable);
+    second_plugin_api *second_api = (second_plugin_api*)sp_get_api(plugin);
+    second_api->my_second_print();
+    
+    sp_unload_plugin(&my_registry,plugin);
+    //We can also unload it trough the API name
+    sp_unload_plugin(&my_registry,SAMPLE_PLUGIN_API_NAME);
+
+
+    //
+    //Hot Reloading
+    //
+    
+    //Let's load up the sample_plugin again to demonstrate hot-reloading
+    sp_load_plugin(plugin_name, reloadable);
+    sample_plugin_api *sample_api = (sample_plugin_api*)sp_get_api(SAMPLE_PLUGIN_API_NAME);
+    sample_api->my_print();
+    
     while(1)
     {
         Sleep(100);
-        sp_internal_api_registry_check_reloadable_plugins();
-        pe = (plugin_example_api*)sp_registry.get(PLUGIN_EXAMPLE_API_NAME);
-        pe->my_print();
+        //Run the registry update, it will check all reloadable plugins for changes.
+        sp_update(); 
+        //We need to get the api again in case the plugin has been modified
+        sample_plugin_api *sample_api = (sample_plugin_api*)sp_get_api(SAMPLE_PLUGIN_API_NAME);
+        sample_api->my_print();
+        
 
+        //More code here...
+        
     }
 
     return 0;
